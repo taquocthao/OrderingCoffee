a6e005c9406dd41526891559f1fc42c0fcdab66c
taquocthao <taquocthao@gmail.com>
2018-03-15 18:02:19 +0700

    tạo layout đăng nhập (chưa hoàn thiện)

--- app/src/main/java/com/tathao/orderingcoffee/LoginActivity.java
+++ app/src/main/java/com/tathao/orderingcoffee/LoginActivity.java
@@ -1,13 +1,15 @@
 package com.tathao.orderingcoffee;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
     }
 }
